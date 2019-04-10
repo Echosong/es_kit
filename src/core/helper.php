4deb76dbe255b469429db7ead46f8ad7514f8d59
echosong <313690636@qq.com>
2019-04-10 10:43:25 +0800

    shell 的操作权限跟web不一样，所以需要区分
    shell 的操作权限跟web不一样，所以需要区分

--- src/core/helper.php
+++ src/core/helper.php
@@ -184,11 +184,12 @@ Class Helper
      */
     public static function log($errMsg, $level = 'info')
     {
-          if(!is_string($errMsg)){
+       if(!is_string($errMsg)){
             $errMsg = json_encode($errMsg);
         }
         //shell 的操作权限跟web不一样，所以需要区分
-        $level = (isset($_REQUEST['m'])?$_REQUEST['m']:'').$level;
+        $logPath = APP_DIR . DS . $GLOBALS['logPath'] . DS . $level .
+            "_".(isset($_REQUEST['m'])?$_REQUEST['m']:''). date('Ymd') . ".log";
         $logPath = APP_DIR . DS . $GLOBALS['logPath'] . DS . $level . "_" . date('Ymd') . ".log";
         error_log(date('Ymd H:i:s') . "  " . $errMsg . PHP_EOL, 3, $logPath);
         if (strtolower(trim($level)) === 'fatal_error') {
